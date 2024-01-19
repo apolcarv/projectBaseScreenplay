af227b7d19327f28a262a2e5cfa1f90748c8d4c4
apolcarv <alejandropolocarvajal@gmail.com>
2024-01-19 10:35:09 -0500

    se actualiza repo base

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # projectBaseScreenplay
 Se crea proyecyo base con patron de diseño screenplay  - actualizado 2024
 
-# ERROR
-El  error ya fue solucionado, solo debia agregar
-### --remote-allow-origins=*;
-En las opciones del  navegador, pero antes de eso debia darle
-el formato correcto y configuracion al archivo serenity.conf
+# Tener presente
+### Se utilizo esta configuración debido a que chromo presenta inestabilidad
+### El driver se auto descarga
+### driver.edge -- navegador web que utilizaremos
+### edgeOptions -- capabilitis o opciones que enviamos para que nuestro navegador maximize, cierre alertas o popup, etc.
+
+# Paso de ejeucion
+#### Buscar la clase TestRunnerTestBase.java
+#### Ejecutar desde la linea 15
--- src/main/java/co/com/screenplay/project/Main.java
+++ src/main/java/co/com/screenplay/project/Main.java
@@ -1,7 +1,51 @@
 package co.com.screenplay.project;
 
+
 public class Main {
+
+    /*
+     * Actividad - Desarrollar un algoritmo básico en el lenguaje de programación que domine
+     *  el candidato (Ordenar un arreglo de enteros de menor a mayor sin usar funcionales como
+     *  Arrays.sort())
+     * */
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        // Arreglo de enteros desordenado
+        int[] arreglo = {5, 2, 8, 1, 6};
+
+        // Llamar al método de ordenamiento
+        ordenarArreglo(arreglo);
+
+        System.out.println("Arreglo ordenado:");
+        for (int elemento : arreglo) {
+            System.out.print(elemento + " ");
+        }
+    }
+
+    // Método de ordenamiento de burbuja
+    public static void ordenarArreglo(int[] arreglo) {
+        int n = arreglo.length;
+        boolean intercambio;
+
+        do {
+            intercambio = false;
+
+            for (int i = 0; i < n - 1; i++) {
+                if (arreglo[i] > arreglo[i + 1]) {
+                    // Intercambiar elementos si están en el orden incorrecto
+                    int temp = arreglo[i];
+                    arreglo[i] = arreglo[i + 1];
+                    arreglo[i + 1] = temp;
+
+                    intercambio = true;
+                }
+            }
+
+            // Reducir la cantidad de elementos a comparar en cada iteración
+            n--;
+
+        } while (intercambio);
     }
 }
+
